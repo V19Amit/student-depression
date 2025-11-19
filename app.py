91a625fa7404610d577a053d8d2f3c0ce86eb036
Amit Verma <sumitverma2001v@gmail.com>
2025-11-19 14:02:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,9 +23,10 @@ with st.sidebar:
     st.title("🧠 Student Depression Predictor")
     st.markdown("Predict the likelihood of depression in students.")
     st.markdown("---")
-    st.markdown("👨‍💻 Developed by: **Brajesh Ahirwar**")
-    st.markdown("🔗 GitHub: [Brajesh Ahirwar](https://github.com/brajesh2306)")
-    st.markdown("🔗 LinkedIn: [Profile](www.linkedin.com/in/brajesh-ahirwar-6269b728b)")
+    st.markdown("👨‍💻 Developed by: **Amit verma**")
+    st.markdown("🔗 GitHub: [Amit verma](https://github.com/V19Amit)")
+    st.markdown("🔗 LinkedIn: [Profile](https://www.linkedin.com/in/amit-verma-b75373326?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app
+)")
     st.markdown("---")
     st.markdown("✨ **Have fun exploring AI!**")
 
@@ -145,3 +146,4 @@ if st.button("Predict"):
 
         except Exception as e:
             st.error(f"Error during prediction: {e}")
+
